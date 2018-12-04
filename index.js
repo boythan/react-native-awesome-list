b1cb19ab2c4ce225ff50ff8df31ac28f9cb44e51
Vinh-Mac <vinh@abivin.com>
2018-12-04 23:35:50 +0700

    https://github.com/boythan/AwesomeList.git git commit index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import AwesomeListComponent from './src/AwesomeListComponent';
-import * as AwesomeListMode from './src/AwesomeListMode'
+import AwesomeListMode from './src/AwesomeListMode'
 import EmptyView from './src/EmptyView'
 export default AwesomeListComponent
 export { AwesomeListMode, EmptyView }
