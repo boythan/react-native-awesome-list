c720f3ebfb0cc0970168a66a721544a37a3bde03
Vinh-Mac <vinh@abivin.com>
2018-12-04 21:47:24 +0700

    export mode and empty view

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
 import AwesomeListComponent from './src/AwesomeListComponent';
-
+import * as AwesomeListMode from './src/AwesomeListMode'
+import EmptyView from './src/EmptyView'
 export default AwesomeListComponent
+export { AwesomeListMode, EmptyView }
