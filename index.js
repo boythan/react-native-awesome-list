3edd3988d334facea2016d8b0778d953634cc2e1
vinhdx <vinhdx.k57@gmail.com>
2022-03-04 18:07:28 +0700

    HARD FOLK

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+/**
+ * @format
+ */
 
-import AwesomeListComponent from './src/AwesomeListComponent';
-import AwesomeListMode from './src/AwesomeListMode'
-import EmptyView from './src/EmptyView'
-export default AwesomeListComponent
-export { AwesomeListMode, EmptyView }
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
